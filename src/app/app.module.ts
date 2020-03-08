d71ab7ef1d51d44e4573edd737aa7105e973c109
ha dao <daogiangthuha1@gmail.com>
2020-03-08 22:38:52 +0700

    Updated login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TodoAddComponent } from './components/todo-add/todo-add.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoAddComponent
+    TodoAddComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule
